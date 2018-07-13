24824ee7e96c3146bf845178574804c7f02c3d14
LiNKeR <am.usa.to.be@gmail.com>
2018-07-13 01:03:47 +0100

    Initial commit

---
+++ app/src/main/java/linkersoft/blackpanther/bubblingimageview/Bubbling.java
@@ -0,0 +1,13 @@
+package linkersoft.blackpanther.bubblingimageview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bubbling extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bubbling);
+    }
+}
